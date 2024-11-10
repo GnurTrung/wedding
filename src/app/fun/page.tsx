54c6790a00fca0db4b686f1efaccbfbac048ce8b
GnurTrunG <trungvv@ftech.ai>
2024-11-10 20:21:59 +0700

    update

--- src/app/fun/page.tsx
+++ src/app/fun/page.tsx
@@ -1,52 +1,28 @@
-"use client";
-import { ApplicationProvider } from "@/context/useApplication";
-import Loading from "../loading";
-import { Toaster } from "react-hot-toast";
-import Header from "@/components/Header";
-import FirstPage from "@/components/FirstPage";
-import RunningText from "@/components/RunningText";
-import SecondPage from "@/components/SecondPage";
-import Gallery from "@/components/Gallery";
-import Footer from "@/components/Footer";
-import Aos from "aos";
-import { useEffect, useState } from "react";
-import SecondPageFun from "@/components/SecondPageFun";
+import FunContainer from "@/container/pageFun";
 
+export const generateMetadata = async () => {
+  const title = "TâmKhương - We are getting married!";
+  const desc = "TâmKhương's wedding invitation";
+  const img = "/assets/images/chew/cuoi_pc.jpg";
+  try {
+    const metadata = {
+      title: title,
+      description: desc,
+      keywords: ["wedding", "tam", "khuong"],
+      openGraph: {
+        title: title,
+        description: desc,
+        images: img,
+      },
+    };
+    return metadata;
+  } catch (error: any) {
+    console.error("Fail");
+    return {};
+  }
+};
 const Fun = () => {
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 1500);
-    Aos.init({
-      offset: 200,
-      duration: 600,
-      easing: "ease-in-sine",
-      once: true,
-    });
-  }, []);
-
-  return (
-    <div>
-      {" "}
-      {isLoading ? (
-        <Loading />
-      ) : (
-        <>
-          <ApplicationProvider>
-            <Toaster />
-            <Header />
-            <FirstPage />
-            <RunningText />
-            <SecondPageFun />
-            <Gallery />
-            <Footer />
-          </ApplicationProvider>
-        </>
-      )}
-    </div>
-  );
+  return <FunContainer />;
 };
 
 export default Fun;
